c07225b154342a30445860f5cb5841efc80191f0
Jauhar Muhammed <jauharmuhammedk@gmail.com>
2025-01-11 15:45:51 +0530

    eslint config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,10 +7,45 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    env: {
+      es2021: true,
+      node: true,
+    },
+  },
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    env: {
+      node: true,
+      es2021: true
+    },
+    rules: {
+      "import/order": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/display-name": "off",
+      "@typescript-eslint/no-unused-vars": "warn"
+    }
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "next/core-web-vitals",
+    "next/typescript"
+  ),
 ];
 
 export default eslintConfig;
